12bd39180f456255b696f4ff3e2cd6fefbeaa475
yash hingu <yashhingu240303@gmail.com>
2024-05-23 01:28:13 -0600

    Created Home page
    Today I have created the front end part of the Main buyer side Home page designing in the code but after a while now my mind is not working so rest some images import part I will work in the next week along with the work given by my team leader for the next week

---
+++ src/app/Home/Home.js
@@ -0,0 +1,251 @@
+export default function Home() {
+    return (
+        <div className="w-full relative bg-white h-[1024px] overflow-hidden text-center text-xl text-white font-inria-sans">
+      <i className="absolute top-[108px] left-[544px] text-21xl inline-block font-bold text-black1 w-[351px] h-14">
+        FABRIC PRODUCTS
+      </i>
+      <div className="absolute top-[196px] left-[84px] w-[243px] h-72">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-100 w-[242px] h-[201px]" />
+        <img
+          className="absolute top-[calc(50%_-_121.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <div className="absolute top-[173px] left-[45px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[217px] left-[17px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[595px] left-[85px] w-[243px] h-[286px]">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-200 w-[242px] h-[201px]" />
+        <img
+          className="absolute top-[calc(50%_-_120.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <div className="absolute top-[171px] left-[50px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[215px] left-[16px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[591px] left-[769px] w-[243px] h-[290px]">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-200 w-[242px] h-[201px]" />
+        <img
+          className="absolute top-[calc(50%_-_122.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <div className="absolute top-[171px] left-[50px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[219px] left-[13px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[595px] left-[1112px] w-[243px] h-[286px]">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-200 w-[242px] h-[201px]" />
+        <img
+          className="absolute top-[calc(50%_-_120.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <div className="absolute top-[175px] left-[50px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[215px] left-[22px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[595px] left-[426px] w-[243px] h-[286px]">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-200 w-[242px] h-[201px]" />
+        <img
+          className="absolute top-[calc(50%_-_120.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <div className="absolute top-[171px] left-[52px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[215px] left-[17px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[196px] left-[426px] w-[243px] h-72">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-200 w-[242px] h-[201px]" />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <img
+          className="absolute top-[calc(50%_-_121.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[173px] left-[52px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[217px] left-[13px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[196px] left-[1112px] w-[243px] h-72">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-200 w-[242px] h-[201px]" />
+        <img
+          className="absolute top-[calc(50%_-_121.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <div className="absolute top-[173px] left-[50px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[217px] left-[12px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[196px] left-[769px] w-[243px] h-72">
+        <div className="absolute top-[0px] left-[1px] rounded-mini bg-lavenderblush-200 w-[242px] h-[201px]" />
+        <div className="absolute top-[167px] left-[0px] rounded-t-none rounded-b-mini bg-black1 w-[243px] h-[38px]" />
+        <img
+          className="absolute top-[calc(50%_-_121.8px)] left-[calc(50%_-_88.5px)] rounded-mini w-[178px] h-[119.5px] object-cover"
+          alt=""
+          src="/547953-9c2st-8746-001-082-0000-lightguccisavoymediumdufflebag-1@2x.png"
+        />
+        <div className="absolute top-[173px] left-[51px] w-[143px] h-[26px]">
+          <i className="absolute top-[0px] left-[35px] inline-block font-bold w-[108px] h-[26px]">
+            Add to cart
+          </i>
+          <img
+            className="absolute top-[0px] left-[0px] w-[26px] h-[26px] object-cover"
+            alt=""
+            src="/shopping-cart@2x.png"
+          />
+        </div>
+        <div className="absolute top-[217px] left-[17px] text-base leading-[24px] font-medium font-title-16px-medium text-left inline-block w-[210px] h-[71px] text-black1">
+          <p className="m-0">{`Cotton DK Yarn Box Set `}</p>
+          <p className="m-0">Bundle (Taupe)</p>
+          <p className="m-0 text-red">$74.95</p>
+        </div>
+      </div>
+      <div className="absolute top-[195.5px] left-[378.5px] box-border w-px h-[698px] border-r-[1px] border-solid border-black1" />
+      <div className="absolute top-[195.5px] left-[1061.5px] box-border w-px h-[698px] border-r-[1px] border-solid border-black1" />
+      <div className="absolute top-[195.5px] left-[720.5px] box-border w-px h-[698px] border-r-[1px] border-solid border-black1" />
+      <div className="absolute top-[525.5px] left-[85.5px] box-border w-[1271px] h-px border-t-[1px] border-solid border-black1" />
+      <img
+        className="absolute top-[86px] left-[0px] max-h-full w-[1440px]"
+        alt=""
+        src="/line-5.svg"
+      />
+      <div className="absolute top-[0px] left-[0px] bg-black1 w-[1440px] h-[81px] overflow-hidden text-13xl">
+        <b className="absolute top-[12px] left-[74px] text-[45px] inline-block w-[298px] h-[57px]">
+          TECH TEXTILE
+        </b>
+        <i className="absolute top-[18px] left-[803px] inline-block font-light w-[122px] h-11">
+          Home
+        </i>
+        <i className="absolute top-[18px] left-[947px] inline-block font-light w-[122px] h-11">
+          Contact
+        </i>
+        <i className="absolute top-[18px] left-[1091px] inline-block font-light w-[122px] h-11">
+          About
+        </i>
+        <div className="absolute top-[19px] left-[482px] rounded-mini bg-gray-200 box-border w-[297px] h-[43px] border-[2px] border-solid border-white" />
+        <i className="absolute top-[29px] left-[494px] text-lg inline-block font-light text-gray-300 w-52 h-[23px]">
+          What are you looking for ?
+        </i>
+        <i className="absolute top-[30px] left-[1287px] text-lg flex font-bold items-center justify-center w-[132px] h-[21px]">
+          SIGN UP/LOG IN
+        </i>
+        <div className="absolute top-[23px] left-[24px] w-[53.2px] h-[33.3px] text-left text-11xl font-chango">
+          <div className="absolute top-[2.5px] left-[0px] tracking-[0.03em] leading-[24px] inline-block w-[26.2px] h-[22.3px] [transform:_rotate(-5.6deg)] [transform-origin:0_0]">
+            T
+          </div>
+          <div className="absolute top-[10px] left-[26px] tracking-[0.03em] leading-[24px] inline-block w-[27.2px] h-[20.8px] [transform:_rotate(5.6deg)] [transform-origin:0_0]">
+            T
+          </div>
+        </div>
+        <div className="absolute top-[30.5px] left-[10px] box-border w-[55px] h-[55px] [transform:_rotate(-30deg)] [transform-origin:0_0] border-[2px] border-solid border-white" />
+        <div className="absolute top-[3px] left-[37.5px] box-border w-[55px] h-[55px] [transform:_rotate(30deg)] [transform-origin:0_0] border-[2px] border-solid border-white" />
+        <img
+          className="absolute top-[19px] left-[1223px] w-[43px] h-[43px] object-cover"
+          alt=""
+          src="/shopping-cart@2x.png"
+        />
+        <img
+          className="absolute top-[29px] left-[747px] w-[23px] h-[23px] object-cover"
+          alt=""
+          src="/search@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
