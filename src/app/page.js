f37ac95594a0f07998734e3cc80437d8cab23f38
Henil-Patel12 <henilkumar.patel@edu.sait.ca>
2024-05-23 21:52:48 -0600

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Header from './Components/Navbar';
+import Footer from './Components/Footer';
 const App = () => {
   return (
     <div>
-      
+      <Header/>
       <main>
         <h2>Welcome to Tech Textile</h2>
         
       </main>
-     
+      <Footer/>
     </div>
   );
 };
