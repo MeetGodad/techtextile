9f182d78b342fcb4111f3ecbcbea5254e23926fb
Meet Patel <meetgodad91048@gmail.com>
2024-05-28 17:04:56 -0600

    Fixed Error

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import Home from "./Home";
-import Header from "../components/Navbar";
-import Footer from "../components/Footer";
+import Home from "./Home/Home";
+import Header from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 export default function Page() {
