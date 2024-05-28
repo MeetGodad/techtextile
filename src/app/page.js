f088e6f58f6b3b11d8a34aba1b30e89e1d4d95b7
yash hingu <yashhingu240303@gmail.com>
2024-05-27 23:11:52 -0600

    Home page random data tiral
    In this code I have tried to get the stuff with all the thing for around 5 hours an now i am come to the conclusion that all the things in my front end  part are working just left with the real random database to go through other than that all the things for the add to cart and teh data fetching all the things are working

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
 
 import Login from "./Login/Login"
+<<<<<<< Updated upstream
 
 
 
+=======
+import Home from "./Home/Home";
+>>>>>>> Stashed changes
 export default function Page() {
 
   return (
     <div>
+<<<<<<< Updated upstream
       <SignUp />
+=======
+      <Home />
+>>>>>>> Stashed changes
     </div>
   );
 }
