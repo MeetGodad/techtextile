01b8a6b53dc4498feb70c8f1f5ae287b682e888e
Henil-Patel12 <henilkumar.patel@edu.sait.ca>
2024-06-12 11:27:32 -0600

    Seller Page 4 week long error solved and buyer profile start working

--- src/app/api/seller/[id]/route.js
+++ src/app/api/seller/[id]/route.js
@@ -1,35 +1,3 @@
-// import { neon } from "@neondatabase/serverless"; 
-
-
-// export async function GET(req, { params }) {
-
-//     try{
-
-//         const id = params.id;  
-//         const databaseUrl = process.env.DATABASE_URL || "";
-//         const sql = neon(databaseUrl);
-
-//         const sellerId = await sql`
-//         SELECT seller_id FROM sellers WHERE user_id = ${id}`
-        
-//         const products = await sql`
-//         SELECT * FROM Products WHERE seller_id = ${sellerId[0].seller_id}`;
-    
-//         if (products.length === 0) {
-//             return new Response(JSON.stringify({ message: "No products found" }), { status: 404 });
-//         }
-
-//         console.log("Response Seller Product :" , products);
-//         return new Response(JSON.stringify(products), { status: 200 });
-
-//     } catch (error) {
-//         return new Response(JSON.stringify({
-//             message: "Internal Server Error",
-//             error: error.message
-//         }), { status: 500 });
-//     }
-// }
-
 import { neon } from '@neondatabase/serverless';
 
 export async function GET(req) {
